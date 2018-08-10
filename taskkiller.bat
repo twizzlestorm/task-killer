24e30a289dd11c4dc010ea2c596149c760d8c758
Ethan <twizzlestorm@gmail.com>
2018-08-10 11:27:26 -0400

    Added tasklist feature
    Also added a copyright thing at the top

--- taskkiller.bat
+++ taskkiller.bat
@@ -1,11 +1,16 @@
+:: (C) Twizzlestorm 2018
 @echo off
 :welcome
 cls
 echo Welcome to TaskKiller
+echo Created by Twizzlestorm
 echo.
 echo Type in a process name to kill (example: chrome.exe) and hit ENTER
-set /p process=Kill:
+echo Alternatively, type "tasklist" to show a list of tasks in case you are not sure what the name of the program is
+set /p process=Kill: 
+echo.
 if %process% == colorhax goto egg1
+if %process% == tasklist goto list
 goto kill
 :kill
 cls
@@ -13,6 +18,12 @@ echo Killing process
 taskkill /f /im %process%
 echo Process was killed. If an error message has appeared, you may have misspelled the name of the program.
 echo Press any key to return to the menu.
+pause >nul
+goto welcome
+:list
+cls
+tasklist
+echo Return to the home screen by hitting any key.
 pause >nul
 goto welcome
 :egg1
